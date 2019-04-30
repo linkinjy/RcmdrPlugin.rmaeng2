81588408edc7a5b1047cff69cc122d97b7daa1d7
Choi Jin Young <linkinjy@nate.com>
2019-04-30 22:15:49 +0900

    111

--- R/internal.R
+++ R/internal.R
@@ -1,11 +1,20 @@
 .onAttach <- function(libname, pkgname){
-  if (!interactive()){
-    return()
+  if (!interactive()) return()
+  Rcmdr <- options()$Rcmdr
+  plugins <- Rcmdr$plugins
+  if (!pkgname %in% plugins) {
+    Rcmdr$plugins <- c(plugins, pkgname)
+    options(Rcmdr=Rcmdr)
+    if("package:Rcmdr" %in% search()) {
+      if(!getRcmdr("autoRestart")) {
+        closeCommander(ask=FALSE, ask.save=TRUE)
+        Commander()
+      }
+    }
+    else {
+      Commander()
+    }
   }
-  else{
-    Commander()
-  }
-
 }
 
 if(getRversion() >= "2.15.1") globalVariables(c("top","button.command"))
