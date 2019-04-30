c8d43a2acc7ba8e9bf60335703db84bf1b46f2f2
Choi Jin Young <linkinjy@nate.com>
2019-04-30 23:00:23 +0900

    ss

--- R/anova_menu.R
+++ R/anova_menu.R
@@ -1,3 +1,23 @@
+.onAttach <- function(libname, pkgname){
+  if (!interactive()) return()
+  putRcmdr("slider.env", new.env())
+  Rcmdr <- options()$Rcmdr
+  plugins <- Rcmdr$plugins
+  if (!pkgname %in% plugins) {
+    Rcmdr$plugins <- c(plugins, pkgname)
+    options(Rcmdr=Rcmdr)
+    if("package:Rcmdr" %in% search()) {
+      if(!getRcmdr("autoRestart")) {
+        closeCommander(ask=FALSE, ask.save=TRUE)
+        Commander()
+      }
+    }
+    else {
+      Commander()
+    }
+  }
+}
+
 anova_menu <- function(list.info=list()){
   ##########################
   ## PREAMBLE/INFORMATION ##
